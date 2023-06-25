0ae4534eb76b957e3c67fc226f2720b0a0eea68e
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2023-06-25 14:05:17 -0300

    Add map concurrent for trans queries. Optimize unliners

--- hsentencer/app/Main.hs
+++ hsentencer/app/Main.hs
@@ -1,46 +1,45 @@
--- {-# LANGUAGE ExtendedDefaultRules #-}
--- {-# OPTIONS_GHC -fno-warn-type-defaults #-}
-
 module Main (main) where
 
 import qualified Shelly as S
+import Shelly (Sh)
 import qualified Data.Text as T
 import Data.Text (Text)
--- import qualified Data.Text.IO as TIO
 -- import Text.Pretty.Simple
 import Control.Lens
-import Data.Aeson.Lens
--- import Data.Aeson
--- import Data.Aeson.Lens
-import Data.Foldable ( Foldable(toList), foldrM )
--- import Data.Traversable
+import Data.Aeson.Lens ( nth, AsValue(_String, _Array) )
+import Data.Foldable ( Foldable(toList))
+import Control.Concurrent.Async (mapConcurrently)
+
 
 main :: IO ()
 main = do
   S.shelly $ S.print_stdout False $ do
     txt <- S.readfile "../sample.txt"
-    result <- foldrM translator T.empty (T.lines txt)
-  -- pPrint result
-    S.writefile "../result.txt" result
+    result <- S.liftIO $ mapConcurrently translator (T.lines txt)
+    -- S.liftIO $ pPrint result
+    S.writefile "../result.txt" $ T.concat result
 
 -- Helpers
 
-translator :: Text -> Text -> S.Sh Text
-translator source  acc = do
-  dump <- S.run "trans" ["es:ru", "-dump", source]
-  -- S.liftIO $ pPrint $ normalizer dump
-  let target = T.unlines $ foldTranslation $ normalizer dump
-  pure $ target <> "\n" <> acc
+translator :: Text -> IO Text
+translator source = do
+  dump <- S.shelly $ runEsRu source
+  pure $ foldTranslation $ normalizer dump
 
+runEsRu :: Text -> Sh Text
+runEsRu source =
+  S.silently $ S.command "trans" ["es:ru", "-dump"] [source]
 
 normalizer :: Text -> Text
 normalizer = T.dropWhileEnd (/=']') . T.dropWhile (/= '[')
 
-foldTranslation :: Text -> [Text]
+foldTranslation :: Text -> Text
 foldTranslation t =
   foldrOf
     (_Just . _Array . folded . _Array)
-    (\(toList -> a) acc ->
-        reverse (a ^.. taking 2 folded . _String) <> acc)
-    []
+    (\(toList -> a) acc
+        -> T.unlines (reverse $ a ^.. taking 2 folded . _String)
+        <> "\n"
+        <> acc)
+    T.empty
     (t ^? nth 0)
