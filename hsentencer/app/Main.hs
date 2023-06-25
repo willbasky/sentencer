22cd07b5956fc62de79631ac4814fbd2e0ea18e2
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2023-06-24 22:57:02 -0300

    Move swapping into foldTranslation

--- hsentencer/app/Main.hs
+++ hsentencer/app/Main.hs
@@ -1,43 +1,46 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ExtendedDefaultRules #-}
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+-- {-# LANGUAGE ExtendedDefaultRules #-}
+-- {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
 module Main (main) where
 
-import Shelly qualified as S
+import qualified Shelly as S
 import qualified Data.Text as T
+import Data.Text (Text)
 -- import qualified Data.Text.IO as TIO
 -- import Text.Pretty.Simple
 import Control.Lens
 import Data.Aeson.Lens
-import Data.Foldable
-import Data.Traversable
+-- import Data.Aeson
+-- import Data.Aeson.Lens
+import Data.Foldable ( Foldable(toList), foldrM )
+-- import Data.Traversable
 
 main :: IO ()
 main = do
   S.shelly $ S.print_stdout False $ do
     txt <- S.readfile "../sample.txt"
-    let texts = T.lines txt
-    results <- forM texts $ \t -> do
-      dump <- S.run "trans" ["es:ru", "-dump", t]
-      pure $ T.unlines $ pairSwap $ foldTranslation $ normalizer dump
-    S.writefile "../result.txt" $ T.unlines results
+    result <- foldrM translator T.empty (T.lines txt)
+  -- pPrint result
+    S.writefile "../result.txt" result
 
 -- Helpers
 
-pairSwap :: [a] -> [a]
-pairSwap [] = []
-pairSwap [a] = [a]
-pairSwap (a:b:ss) = b : a : pairSwap ss
+translator :: Text -> Text -> S.Sh Text
+translator source  acc = do
+  dump <- S.run "trans" ["es:ru", "-dump", source]
+  -- S.liftIO $ pPrint $ normalizer dump
+  let target = T.unlines $ foldTranslation $ normalizer dump
+  pure $ target <> "\n" <> acc
 
-normalizer :: T.Text -> T.Text
+
+normalizer :: Text -> Text
 normalizer = T.dropWhileEnd (/=']') . T.dropWhile (/= '[')
 
-foldTranslation :: T.Text -> [T.Text]
-foldTranslation t = (t ^? nth 0)
-            ^.. _Just
-            . _Array
-            . folded
-            . _Array
-            . taking 2 folded
-            . _String
+foldTranslation :: Text -> [Text]
+foldTranslation t =
+  foldrOf
+    (_Just . _Array . folded . _Array)
+    (\(toList -> a) acc ->
+        reverse (a ^.. taking 2 folded . _String) <> acc)
+    []
+    (t ^? nth 0)
