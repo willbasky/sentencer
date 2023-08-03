968b553d0eb4e9f1982dd49916d7b9f66465bc1d
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2023-08-03 01:09:51 -0300

    Rust: mvp implemented

--- rsentencer/src/main.rs
+++ rsentencer/src/main.rs
@@ -2,7 +2,7 @@ mod util;
 
 use util::*;
 
-use std::fs::read_to_string;
+use std::fs::{read_to_string, self};
 
 use serde_json::Value;
 
@@ -10,7 +10,6 @@ use serde_json::Value;
 fn main() {
   let config_str =
     read_to_string("content.toml").unwrap();
-  // println!("{:?}",&config_str);
 
   let conf: Config = toml::from_str(&config_str).unwrap();
   println!("{:?}",conf);
@@ -19,30 +18,33 @@ fn main() {
 
   let source_lines = read_lines(&input_path);
 
-  // for source in source_lines {
-  //   let dump = runEsRu(&source);
-  // }
-  let dump = runEsRu(&source_lines[0]);
-  let dump_normalized = normalaizer(&dump);
-  println!("dump:\n{}", &dump);
-  println!("dump normal:\n{}", &dump_normalized);
+  let mut translation: Vec<String> = Vec::new();
 
-  let v: Value = serde_json::from_str(&dump_normalized).unwrap();
-  println!("value:\n{}", &v[0]);
+  for source in source_lines {
 
-  let v_iter = v[0].as_array().unwrap();
+    let dump = runEsRu(&source);
+    let dump_normalized = normalaizer(&dump);
 
-  let mut flat_res: Vec<(String, String)> = Vec::new();
-  for arr in v_iter {
-    let source = &arr[1];
-    let target = &arr[0];
-    if target.is_string() && source.is_string() {
-      let s = source.as_str().unwrap().to_string();
-      let t = target.as_str().unwrap().to_string();
-      flat_res.push((s,t))
+    let v: Value = serde_json::from_str(&dump_normalized).unwrap();
+
+    if v[0].is_array() {
+      let v_iter = v[0].as_array().unwrap();
+
+      for arr in v_iter {
+        let source = &arr[1];
+        let target = &arr[0];
+        if target.is_string() && source.is_string() {
+          let s = source.as_str().unwrap().to_string();
+          let t = target.as_str().unwrap().to_string();
+          translation.push(format!("{}\n{}",s,t))
+        }
+      }
     }
   }
-  println!("{:?}", flat_res)
+  let res = translation.join("\n\n");
+
+  fs::write("content/rust_test.txt", res).unwrap()
+
 
 }
 
